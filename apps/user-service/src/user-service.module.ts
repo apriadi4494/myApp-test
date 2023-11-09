0510b4b3ed8a7e927ae8770bf2a0d32277eee55f
apriadi <apriadi4494@gmail.com>
2023-11-10 04:44:47 +0700

    feat(user-service): added mongodb connection

--- apps/user-service/src/user-service.module.ts
+++ apps/user-service/src/user-service.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DB_NAME, DB_URL } from 'libs/src';
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(DB_URL, {
+      dbName: DB_NAME,
+    }),
+  ],
 })
 export class UserServiceModule {}
--- libs/src/config/env/env-config.ts
+++ libs/src/config/env/env-config.ts
@@ -1,5 +1,11 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const APP_HOST: string = process.env.APP_HOST;
 export const APP_PORT: string = process.env.APP_PORT;
 export const APP_NODE: string = process.env.APP_NODE;
+
+export const DB_NAME: string = process.env.DB_NAME;
+export const DB_URL: string = process.env.DB_URL;
 
 export const SWAGGER_URL: string = process.env.SWAGGER_URL;
