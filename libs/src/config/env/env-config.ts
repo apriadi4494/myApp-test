c700d443296114449ee0be79f30b04f83e1a6460
apriadi <apriadi4494@gmail.com>
2023-11-10 03:54:26 +0700

    chore: added env configuration

--- libs/src/config/env/env-config.ts
+++ libs/src/config/env/env-config.ts
@@ -1,3 +1,5 @@
 export const APP_HOST: string = process.env.APP_HOST;
 export const APP_PORT: string = process.env.APP_PORT;
 export const APP_NODE: string = process.env.APP_NODE;
+
+export const SWAGGER_URL: string = process.env.SWAGGER_URL;
