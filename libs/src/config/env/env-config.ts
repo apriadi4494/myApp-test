fb541773df19e4aa05e638aa564d3127f4b8bc9f
apriadi <apriadi4494@gmail.com>
2023-11-10 06:56:57 +0700

    fix: change env jwt name

--- libs/src/config/env/env-config.ts
+++ libs/src/config/env/env-config.ts
@@ -8,4 +8,8 @@ export const APP_NODE: string = process.env.APP_NODE;
 export const DB_NAME: string = process.env.DB_NAME;
 export const DB_URL: string = process.env.DB_URL;
 
+export const JWT_SECRET: string = process.env.JWT_SECRET;
+export const TOKEN_EXPIRED: string = process.env.TOKEN_EXPIRED;
+export const REFRESH_TOKEN_EXPIRED: string = process.env.REFRESH_TOKEN_EXPIRED;
+
 export const SWAGGER_URL: string = process.env.SWAGGER_URL;
