69456aecb20ca4cc30579a0bbb52352e4329adb0
apriadi <apriadi4494@gmail.com>
2023-11-11 11:12:37 +0700

    refactory(user-service): clean the strategy auth jwt

--- libs/src/common/strategy/auth-jwt.strategy.ts
+++ libs/src/common/strategy/auth-jwt.strategy.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { AuthService } from 'apps/user-service/src/modules/auth/auth.service';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'auth-jwt') {
-  constructor(private authService: AuthService) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
